5b9b0787abc81b62ec8b815185afa4eddf47e7f8
C Badweh <sheenaymudwe@gmail.com>
2025-01-29 12:35:10 -0500

    first try to push

--- ci-cd-tools/flashi.bat
+++ ci-cd-tools/flashi.bat
@@ -6,8 +6,8 @@ setlocal
 set "build_type=Debug"
 if not [%1]==[] set "build_type=%1"
 
-set "ws_root=C:\Users\gene\Documents\Gene\proj\STM32CubeIDE\workspace_1.6.1\ci-cd-class-1"
-set "sn=066BFF575550897767162155"
-set "image_file=%ws_root%\%build_type%\ci-cd-class-1.bin"
+set "ws_root=C:\Users\Sheen\Desktop\CICD"
+set "sn=0670FF3632524B3043205426"
+set "image_file=%ws_root%\%build_type%\CICDBadweh.bin"
 
 "%ws_root%\ci-cd-tools\flash.bat" %sn% "%image_file%"
